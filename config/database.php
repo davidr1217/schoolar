5f946500cd223a05c03669d784f0d76a6a991d2d
David <david@gmail.com>
2025-03-21 18:26:43 -0500

    Actualizacion

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+    //Config connection
+    $host       = "localhost";
+    $port       = "5432";
+    $dbname     = "schoolar";
+    $user       = "postgres";
+    $password   = "unicesmag";
+
+    //create connection 
+    $conn = pg_connect("
+        host=$host
+        port=$port
+        dbname=$dbname
+        user=$user
+        password=$password
+    
+    ");
+
+    if (!$conn){
+        die("connection error: " . pg_last_error());
+    }else{
+        echo "Succcess connection";
+    }
+
+?>
