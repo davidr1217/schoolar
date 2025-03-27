5209d8b37d2fc8aa61b4ef32992eda5bb202d9c7
David <david@gmail.com>
2025-03-27 11:22:19 -0500

    Actualizacion

--- config/database.php
+++ config/database.php
@@ -22,4 +22,5 @@
         echo "Succcess connection";
     }
 
+    pg_close();
 ?>
---
+++ src/signum.php
@@ -0,0 +1,22 @@
+<?php
+
+    include('../config/database.php');
+
+    $fname   = $_POST['f_name'];
+    $lname   = $_POST['l_name'];
+    $email   = $_POST['e_mail'];
+    $passwd  = $_POST['passw'];
+
+    $sql = "INSERT INTO users (firstname, lastname, email, pasword)
+                VALUES('$fname','$lname','$email',' $passwd')
+    ";
+
+    $res = pg_query($conn, $sql);
+
+    if($res){
+        echo"User has been created succesfully";
+    }else{
+        echo "ERROR";
+    }
+    
+?>
