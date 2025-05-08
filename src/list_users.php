1f7e3b75ac8d222753a1375cae30b27543308594
David <david@gmail.com>
2025-05-08 11:26:51 -0500

    ACTUALIZAR

---
+++ src/list_users.php
@@ -0,0 +1,39 @@
+<?php
+include('../config/database.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname </th>
+            <th>E-mail</th>
+            <th>Status</th>
+            <th>...</th>
+        </tr>
+        <tr>
+            <td>David</td>
+            <td>Rivas</td>
+            <td>daoip@gmail.com</td>
+            <td>Ative</td>
+            <td>
+                <img src = "icons/lapiz-3d.png" width="15">
+                <img src = "icons/bote-de-basura.png" width="15">
+                <img src = "icons/lupa.png" width="15">
+            </td>
+        </tr>
+    
+        <?php
+        //Hero code
+        $sql = "SELECT";
+        ?>
+</table>
+</body>
+</html>
--- src/logout.php
+++ src/logout.php
@@ -3,5 +3,5 @@
     session_destroy();
 
     header('Refresh: 0; URL=http://localhost/schoolar/src/signin.html');
-    
+
 ?>
